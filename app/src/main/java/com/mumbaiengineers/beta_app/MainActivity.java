3cf01faa50f3a0f255432e9216c698375a36046a
Rohit <patil.rohit048@gmail.com>
2017-12-21 01:23:19 +0530

    Major changes

--- app/src/main/java/com/mumbaiengineers/beta_app/MainActivity.java
+++ app/src/main/java/com/mumbaiengineers/beta_app/MainActivity.java
@@ -7,7 +7,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class  MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button btnAoa, btnCoa, btnOs, btnCg;
     @Override
