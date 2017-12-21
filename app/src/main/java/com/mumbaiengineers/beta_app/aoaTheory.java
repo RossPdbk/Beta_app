6186bc6e0e2f7026106f88222a415de9f9897265
Shubham <rosspdbk@gmail.com>
2017-12-22 01:04:01 +0530

    22-12-17       01:03

--- app/src/main/java/com/mumbaiengineers/beta_app/aoaTheory.java
+++ app/src/main/java/com/mumbaiengineers/beta_app/aoaTheory.java
@@ -5,33 +5,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class aoaTheory extends AppCompatActivity implements View.OnClickListener {
-    Button aoaTheoryQ1, aoaTheoryQ2, aoaTheoryQ3, aoaTheoryQ4, aoaTheoryQ5, aoaTheoryQ6, aoaTheoryQ7, aoaTheoryQ8, aoaTheoryQ9, aoaTheoryQ10;
-    Button aoaTheoryQ11, aoaTheoryQ12, aoaTheoryQ13, aoaTheoryQ14, aoaTheoryQ15, aoaTheoryQ16, aoaTheoryQ17, aoaTheoryQ18;
+    TextView aoaTheoryQ1, aoaTheoryQ2, aoaTheoryQ3, aoaTheoryQ4, aoaTheoryQ5, aoaTheoryQ6, aoaTheoryQ7, aoaTheoryQ8, aoaTheoryQ9, aoaTheoryQ10;
+    TextView aoaTheoryQ11, aoaTheoryQ12, aoaTheoryQ13, aoaTheoryQ14, aoaTheoryQ15, aoaTheoryQ16, aoaTheoryQ17, aoaTheoryQ18;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aoa_theory);
-        aoaTheoryQ1 = (Button) findViewById(R.id.aoaTheoryQ1);
-        aoaTheoryQ2 = (Button) findViewById(R.id.aoaTheoryQ2);
-        aoaTheoryQ3 = (Button) findViewById(R.id.aoaTheoryQ3);
-        aoaTheoryQ4 = (Button) findViewById(R.id.aoaTheoryQ4);
-        aoaTheoryQ5 = (Button) findViewById(R.id.aoaTheoryQ5);
-        aoaTheoryQ6 = (Button) findViewById(R.id.aoaTheoryQ6);
-        aoaTheoryQ7 = (Button) findViewById(R.id.aoaTheoryQ7);
-        aoaTheoryQ8 = (Button) findViewById(R.id.aoaTheoryQ8);
-        aoaTheoryQ9 = (Button) findViewById(R.id.aoaTheoryQ9);
-        aoaTheoryQ10 = (Button) findViewById(R.id.aoaTheoryQ10);
-        aoaTheoryQ11 = (Button) findViewById(R.id.aoaTheoryQ11);
-        aoaTheoryQ12 = (Button) findViewById(R.id.aoaTheoryQ12);
-        aoaTheoryQ13 = (Button) findViewById(R.id.aoaTheoryQ13);
-        aoaTheoryQ14 = (Button) findViewById(R.id.aoaTheoryQ14);
-        aoaTheoryQ15 = (Button) findViewById(R.id.aoaTheoryQ15);
-        aoaTheoryQ16 = (Button) findViewById(R.id.aoaTheoryQ16);
-        aoaTheoryQ17 = (Button) findViewById(R.id.aoaTheoryQ17);
-        aoaTheoryQ18 = (Button) findViewById(R.id.aoaTheoryQ18);
+        aoaTheoryQ1 = (TextView) findViewById(R.id.aoaTheoryQ1);
+        aoaTheoryQ2 = (TextView) findViewById(R.id.aoaTheoryQ2);
+        aoaTheoryQ3 = (TextView) findViewById(R.id.aoaTheoryQ3);
+        aoaTheoryQ4 = (TextView) findViewById(R.id.aoaTheoryQ4);
+        aoaTheoryQ5 = (TextView) findViewById(R.id.aoaTheoryQ5);
+        aoaTheoryQ6 = (TextView) findViewById(R.id.aoaTheoryQ6);
+        aoaTheoryQ7 = (TextView) findViewById(R.id.aoaTheoryQ7);
+        aoaTheoryQ8 = (TextView) findViewById(R.id.aoaTheoryQ8);
+        aoaTheoryQ9 = (TextView) findViewById(R.id.aoaTheoryQ9);
+        aoaTheoryQ10 = (TextView) findViewById(R.id.aoaTheoryQ10);
+        aoaTheoryQ11 = (TextView) findViewById(R.id.aoaTheoryQ11);
+        aoaTheoryQ12 = (TextView) findViewById(R.id.aoaTheoryQ12);
+        aoaTheoryQ13 = (TextView) findViewById(R.id.aoaTheoryQ13);
+        aoaTheoryQ14 = (TextView) findViewById(R.id.aoaTheoryQ14);
+        aoaTheoryQ15 = (TextView) findViewById(R.id.aoaTheoryQ15);
+        aoaTheoryQ16 = (TextView) findViewById(R.id.aoaTheoryQ16);
+        aoaTheoryQ17 = (TextView) findViewById(R.id.aoaTheoryQ17);
+        aoaTheoryQ18 = (TextView) findViewById(R.id.aoaTheoryQ18);
 
         aoaTheoryQ1.setOnClickListener(this);
         aoaTheoryQ2.setOnClickListener(this);
